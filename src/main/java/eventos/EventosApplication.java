0060d811faba2fd7ea70266fd732cf54421ebaef
ruither.borba <delki8@gmail.com>
2022-05-26 20:18:50 -0300

    returning a collection to preserve interface

--- src/main/java/eventos/EventosApplication.java
+++ src/main/java/eventos/EventosApplication.java
@@ -6,7 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.GetMapping;
 import java.time.LocalDateTime;
-
+import java.util.List;
+import java.util.Collections;
 
 import eventos.entities.Evento;
 
@@ -20,8 +21,8 @@ public class EventosApplication {
   }
 
   @GetMapping("/eventos")
-  public Evento eventos(@RequestParam(value = "amount") Integer amount) {
-    return new Evento(
+  public List<Evento> eventos(@RequestParam(value = "amount") Integer amount) {
+    return Collections.singletonList(new Evento(
 		      "acampamento",
 		      LocalDateTime.now(),
 		      LocalDateTime.now().plusDays(7),
@@ -29,7 +30,7 @@ public class EventosApplication {
 		      "A Igreja Presbiteriana Mosaico existe para acolher pessoas e formar discípulos de Cristo através de relacionamentos saudáveis e uma pregação bíblica contemporânea no bairro Setor Bueno, na cidade de Goiânia e no mundo",
 		      "R$ 15,00 adulto R$ 10,00 até 12 anos",
 		      "Na Igreja Presbiteriana Mosaico",
-		      "Rua T-53, 480, Setor Bueno Goiânia/Go - Cep 74810-210");
+		      "Rua T-53, 480, Setor Bueno Goiânia/Go - Cep 74810-210"));
   }
 
 }
